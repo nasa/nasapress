4733aabbfa39c1844b8bcdf1781ce8dc452cbce2
Brandon Ruffridge <bruffridge@nasa.gov>
2018-06-15 15:28:10 -0400

    removed default of "this site" search.

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -3,7 +3,7 @@
     <div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div>
 
     <input id="affiliate" name="affiliate" type="hidden" value="nasa-glenn" />
-    <input id="dc" name="dc" type="hidden" value="4266" />
+    <!--<input id="dc" name="dc" type="hidden" value="4266" />-->
     <label class="usa-sr-only" for="query">Enter Search Term(s):</label>
     <input autocomplete="off" id="query" name="query" type="search" placeholder="Search">
     <button type="submit" name="commit" value="Search">
